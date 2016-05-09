ec636c9573066595a9f0964e90ed88c21592830e
jyoti-sumeru <jyoti.singh@sumerusolutions.com>
2016-05-09 13:18:48 +0530

    Added Style and Js Changes for Auto Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
   def index
     if current_user.user_profile.present?
       @user_profile = current_user.user_profile
-      @tweets = current_user.tweets
+      @tweet = Tweet.new
+      @tweets = current_user.tweets.order("updated_at desc")
     else
       redirect_to new_user_profile_path
     end
